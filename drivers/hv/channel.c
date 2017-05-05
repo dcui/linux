0d4f8d1109181dbef569f03e38758d75bf0e6541
Dexuan Cui <decui@microsoft.com>
2017-05-05 16:57:23 -0600

    UBUNTU: SAUCE: vmbus: dynamically enqueue/dequeue a channel on vmbus_open/close
    BugLink: http://bugs.launchpad.net/bugs/1698425
    
    A just-closed channel may have a pending interrupt, and later when a new
    channel with the same channel ID is not being fully initialized, the
    pending interrupt of the previous channel with the same channel ID can run
    the channel callback on the new channel data structure, causing a crash
    of NULL pointer dereferencing.
    
    Normally it’s pretty hard to reproduce the race condition, but it can
    indeed happen with specially-designed hv_sock stress test cases.
    
    Signed-off-by: Dexuan Cui <decui@microsoft.com>
    Reported-by: Rolf Neugebauer <rolf.neugebauer@docker.com>
    Tested-by: Rolf Neugebauer <rolf.neugebauer@docker.com>
    Cc: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Cc: Stephen Hemminger <sthemmin@microsoft.com>
    Acked-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- drivers/hv/channel.c
+++ drivers/hv/channel.c
@@ -182,6 +182,8 @@ int vmbus_open(struct vmbus_channel *newchannel, u32 send_ringbuffer_size,
 		goto error_free_gpadl;
 	}
 
+	hv_percpu_channel_enq(newchannel);
+
 	ret = vmbus_post_msg(open_msg,
 			     sizeof(struct vmbus_channel_open_channel), true);
 
@@ -194,23 +196,25 @@ int vmbus_open(struct vmbus_channel *newchannel, u32 send_ringbuffer_size,
 
 	if (ret != 0) {
 		err = ret;
-		goto error_free_gpadl;
+		goto error_deq_channel;
 	}
 
 	if (newchannel->rescind) {
 		err = -ENODEV;
-		goto error_free_gpadl;
+		goto error_deq_channel;
 	}
 
 	if (open_info->response.open_result.status) {
 		err = -EAGAIN;
-		goto error_free_gpadl;
+		goto error_deq_channel;
 	}
 
 	newchannel->state = CHANNEL_OPENED_STATE;
 	kfree(open_info);
 	return 0;
 
+error_deq_channel:
+	hv_percpu_channel_deq(newchannel);
 error_free_gpadl:
 	vmbus_teardown_gpadl(newchannel, newchannel->ringbuffer_gpadlhandle);
 	kfree(open_info);
@@ -564,6 +568,8 @@ static int vmbus_close_internal(struct vmbus_channel *channel)
 		ret = -EINVAL;
 		goto out;
 	}
+
+	hv_percpu_channel_deq(channel);
 
 	channel->state = CHANNEL_OPEN_STATE;
 	channel->sc_creation_callback = NULL;
