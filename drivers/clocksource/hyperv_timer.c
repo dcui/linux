5ec3396d5959db40e03dcc46263ea6903f833b2b
Stanislav Kinsburskiy <stanislav.kinsburskiy@gmail.com>
2022-11-03 17:58:54 +0000

    clocksource: hyper-v: Introduce TSC PFN getter
    And rework the code to use it instead of the physical address, which isn't
    required by itself.
    
    This is a cleanup and precursor patch for upcoming support for TSC page
    mapping into Microsoft Hypervisor root partition, where TSC PFN will be
    defined by the hypervisor and not by the kernel.
    
    Signed-off-by: Stanislav Kinsburskiy <stanislav.kinsburskiy@gmail.com>
    CC: "K. Y. Srinivasan" <kys@microsoft.com>
    CC: Haiyang Zhang <haiyangz@microsoft.com>
    CC: Wei Liu <wei.liu@kernel.org>
    CC: Dexuan Cui <decui@microsoft.com>
    CC: Daniel Lezcano <daniel.lezcano@linaro.org>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: linux-hyperv@vger.kernel.org
    CC: linux-kernel@vger.kernel.org
    Reviewed-by: Michael Kelley <mikelley@microsoft.com>
    Reviewed-by: Anirudh Rayabharam <anrayabh@linux.microsoft.com>
    Link: https://lore.kernel.org/r/166749833420.218190.2102763345349472395.stgit@skinsburskii-cloud-desktop.internal.cloudapp.net
    Signed-off-by: Wei Liu <wei.liu@kernel.org>
    (cherry picked from commit e1f5c66db85fb3e0a17b803fc2e868e9c59c193a)
    Signed-off-by: Dexuan Cui <decui@microsoft.com>

--- drivers/clocksource/hyperv_timer.c
+++ drivers/clocksource/hyperv_timer.c
@@ -368,6 +368,12 @@ static union {
 } tsc_pg __aligned(PAGE_SIZE);
 
 static struct ms_hyperv_tsc_page *tsc_page = &tsc_pg.page;
+static unsigned long tsc_pfn;
+
+static unsigned long hv_get_tsc_pfn(void)
+{
+	return tsc_pfn;
+}
 
 struct ms_hyperv_tsc_page *hv_get_tsc_page(void)
 {
@@ -409,13 +415,12 @@ static void suspend_hv_clock_tsc(struct clocksource *arg)
 
 static void resume_hv_clock_tsc(struct clocksource *arg)
 {
-	phys_addr_t phys_addr = virt_to_phys(tsc_page);
 	union hv_reference_tsc_msr tsc_msr;
 
 	/* Re-enable the TSC page */
 	tsc_msr.as_uint64 = hv_get_register(HV_REGISTER_REFERENCE_TSC);
 	tsc_msr.enable = 1;
-	tsc_msr.pfn = HVPFN_DOWN(phys_addr);
+	tsc_msr.pfn = tsc_pfn;
 	hv_set_register(HV_REGISTER_REFERENCE_TSC, tsc_msr.as_uint64);
 }
 
@@ -493,7 +498,6 @@ static __always_inline void hv_setup_sched_clock(void *sched_clock) {}
 static void __init hv_init_tsc_clocksource(void)
 {
 	union hv_reference_tsc_msr tsc_msr;
-	phys_addr_t	phys_addr;
 
 	if (hv_root_partition)
 		return false;
@@ -515,7 +519,7 @@ static void __init hv_init_tsc_clocksource(void)
 		return;
 
 	hv_read_reference_counter = read_hv_clock_tsc;
-	phys_addr = virt_to_phys(hv_get_tsc_page());
+	tsc_pfn = HVPFN_DOWN(virt_to_phys(tsc_page));
 
 	/*
 	 * The Hyper-V TLFS specifies to preserve the value of reserved
@@ -526,7 +530,7 @@ static void __init hv_init_tsc_clocksource(void)
 	 */
 	tsc_msr.as_uint64 = hv_get_register(HV_REGISTER_REFERENCE_TSC);
 	tsc_msr.enable = 1;
-	tsc_msr.pfn = HVPFN_DOWN(phys_addr);
+	tsc_msr.pfn = tsc_pfn;
 	hv_set_register(HV_REGISTER_REFERENCE_TSC, tsc_msr.as_uint64);
 
 	clocksource_register_hz(&hyperv_cs_tsc, NSEC_PER_SEC/100);
