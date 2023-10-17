c4f672554d3a0800db9bd570575ddd559846b81d
Dexuan Cui <decui@microsoft.com>
2023-10-17 23:20:50 +0000

    x86/mm: Print the encryption features correctly when a paravisor is present
    Hyper-V provides two modes for running a TDX/SNP VM:
    
    1) In TD Partitioning mode (TDX) or vTOM mode (SNP) with a paravisor;
    2) In "fully enlightened" mode with the normal TDX shared bit or SNP C-bit
       control over page encryption, and no paravisor.
    
    In the first mode (i.e. paravisor mode), the native TDX/SNP CPUID
    capability is hidden from the VM, but cc_platform_has(CC_ATTR_MEM_ENCRYPT)
    and cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT) are true; as a result,
    mem_encrypt_init() incorrectly prints the below message when an Intel TDX
    VM with a paravisor runs on Hyper-V:
    "Memory Encryption Features active: AMD SEV".
    
    Fix this by calling a new function hv_print_mem_enc_feature_info() if
    hyperv_paravisor_present is true.
    
    Signed-off-by: Dexuan Cui <decui@microsoft.com>

--- arch/x86/mm/mem_encrypt.c
+++ arch/x86/mm/mem_encrypt.c
@@ -75,6 +75,9 @@ static void print_mem_encrypt_feature_info(void)
 	pr_cont("\n");
 }
 
+void hv_print_mem_enc_feature_info(void);
+extern bool hyperv_paravisor_present;
+
 /* Architecture __weak replacement functions */
 void __init mem_encrypt_init(void)
 {
@@ -84,5 +87,8 @@ void __init mem_encrypt_init(void)
 	/* Call into SWIOTLB to update the SWIOTLB DMA buffers */
 	swiotlb_update_mem_attributes();
 
-	print_mem_encrypt_feature_info();
+	if (!hyperv_paravisor_present)
+		print_mem_encrypt_feature_info();
+	else
+		hv_print_mem_enc_feature_info();
 }
